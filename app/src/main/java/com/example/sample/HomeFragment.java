ce531b5f49d6577c4950c30b242d70836759af58
Indresh10 <hemaniindresh@gmail.com>
2021-02-20 02:00:17 +0530

    add card and button

--- app/src/main/java/com/example/sample/HomeFragment.java
+++ app/src/main/java/com/example/sample/HomeFragment.java
@@ -18,11 +18,7 @@ import android.widget.ViewFlipper;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
 
@@ -37,15 +33,6 @@ public class HomeFragment extends Fragment {
 
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -66,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
     void openFragment(Fragment fragment){
         FragmentTransaction FT = getSupportFragmentManager().beginTransaction();
         FT.replace(R.id.frame,fragment);
-        FT.addToBackStack(null);
         FT.commit();
     }
 }
