8490878700b7d52ae80b9dcfe8543e891a46035d
Indresh10 <hemaniindresh@gmail.com>
2021-02-19 20:15:56 +0530

    add banner ads

--- app/src/main/java/com/example/sample/HomeFragment.java
+++ app/src/main/java/com/example/sample/HomeFragment.java
@@ -2,11 +2,21 @@ package com.example.sample;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +25,13 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+
 
     public HomeFragment() {
 
@@ -45,10 +54,14 @@ public class HomeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
+    ViewFlipper flip;
+    LinearLayout dotsLayout;
+    ImageView[] dots = new ImageView[3];
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String mParam1;
+        String mParam2;
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -60,5 +73,47 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view,savedInstanceState);
+        flip = view.findViewById(R.id.carousel);
+        flip.setAutoStart(true);
+        flip.setFlipInterval(4000);
+        flip.setInAnimation(getContext(),R.anim.slide_in);
+        flip.setOutAnimation(getContext(),R.anim.slide_out);
+        dotsLayout = view.findViewById(R.id.dots_layout);
+        setActiveDot(flip.getDisplayedChild());
+        flip.getInAnimation().setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                setActiveDot(flip.getDisplayedChild());
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+    }
+    void setActiveDot(int position){
+        if(dotsLayout.getChildCount() > 0){
+            dotsLayout.removeAllViews();
+        }
+        for(int i=0;i<=2;i++){
+            dots[i] = new ImageView(getContext());
+            if(i==position){
+                dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.active_dot));
+            }else{
+                dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.inactive_dot));
+            }
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(12,0,12,0);
+            dotsLayout.addView(dots[i],layoutParams);
+        }
     }
 }
